261566b1dfb4c53a02f28d79a6e3d9b27d789438
Saurabh Pandey <saurabhpandey50000@gmail.com>
2023-03-15 17:19:25 +0530

    changes

--- app/src/main/java/com/example/yogabot/MainActivity.java
+++ app/src/main/java/com/example/yogabot/MainActivity.java
@@ -2,19 +2,46 @@ package com.example.yogabot;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+    BottomNavigationView navigationView;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        navigationView = findViewById(R.id.navigationBar1);
+        navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment = null;
+                switch (item.getItemId()){
+                    case R.id.home:
+                        fragment = new HomeFragment();
+                        break;
+                    case R.id.chatbot:
+                        fragment = new chatbot();
+                        break;
+                    case R.id.dietplan:
+                        fragment = new dietPlan();
+                        break;
+                    case R.id.profile:
+                        fragment = new userProfile();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.parent_layout , fragment).commit();
+                return true;
+            }
+        });
 
 
 
